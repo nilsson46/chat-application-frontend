9dc2b212db1bf7e4a956f66fded2cc94d17354b0
snilss61 <snilss61@volvocars.com>
2024-05-08 14:03:43 +0200

    Now the user that logged in are connected to the websocket at the same time they log in

--- vue-project/src/components/LoginComponent.vue
+++ vue-project/src/components/LoginComponent.vue
@@ -16,15 +16,58 @@
 </template>
 <script>
 import axios from 'axios';
+import { Client } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+import { mapActions } from 'vuex';
 
 export default {
     data() {
         return {
-            username: '',
-            password: '',
+            user: {
+                username: '',
+                password: ''
+            },
+            websocket:{
+                socketUrl: 'http://localhost:8080',
+                socket: null,
+                client: null,
+                connected: false
+            }
+            
         };
     },
     methods: {
+        ...mapActions(['setConnect']),
+        connectWebSocket() {
+            // Create a WebSocket connection
+            this.websocket.socket = new SockJS(`${this.websocket.socketUrl}/connect`);
+            this.websocket.client = new Client({ webSocketFactory: () => this.websocket.socket });
+
+            // Listen for the event when the WebSocket connection is established
+            this.websocket.client.onConnect = () => {
+                console.log('Connected to WebSocket server');
+                this.websocket.connected = true;
+                this.setConnected(true);
+
+                // Add a listener to receive messages
+                this.websocket.client.subscribe('/topic/public', (message) => {
+                    if (message.body) {
+                        let receivedMessage = JSON.parse(message.body);
+                        console.log('Received message:', receivedMessage);
+                    }
+                });
+            };
+
+            // Listen for the event when the WebSocket connection is terminated
+            this.websocket.client.onDisconnect = () => {
+                console.log('Disconnected from WebSocket server');
+                this.websocket.connected = false;
+                this.setConnected(false);
+            };
+
+            // Activate the WebSocket connection
+            this.websocket.client.activate();
+        },
         loginUser() {
             axios.post('http://localhost:8080/login', {
                 username: this.username,
@@ -36,6 +79,7 @@ export default {
                 console.log(response.data);
                 console.log(response.data.token);
                 // Redirect the user to a different page
+                this.connectWebSocket();
                 this.$router.push('/landing');
             })
             .catch(error => {
