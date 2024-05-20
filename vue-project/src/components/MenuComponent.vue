a2e77ff495a80790847953e01be667a7f507c10d
nilsson46 <nilssonsimon46@gmail.com>
2024-05-20 14:43:21 +0200

    Added a checker for if the user are logged in or not

---
+++ vue-project/src/components/MenuComponent.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+<template>
+    <div>
+      <div v-if="$store.state.isLoggedIn">
+        Loggad
+      </div>
+      <div v-else>
+        Logga in eller registrera dig för att använda appen
+      </div>
+    </div>
+  </template>
