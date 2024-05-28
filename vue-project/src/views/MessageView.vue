bc52250f977674cb766412f5d824cc5c4a64751a
nilsson46 <nilssonsimon46@gmail.com>
2024-05-28 13:57:11 +0200

    Added a switch button on the message so you only can see private or public messages

--- vue-project/src/views/MessageView.vue
+++ vue-project/src/views/MessageView.vue
@@ -1,13 +1,21 @@
 <script setup>
+import { ref } from 'vue';
 import NavBar from '../components/NavBar.vue'
 import Message from '../components/Message.vue'
 import PrivateMessage from '@/components/PrivateMessageComponent.vue';
+const showPrivateMessage = ref(false);
+const toggleChat = () => {
+  showPrivateMessage.value = !showPrivateMessage.value;
+};
 </script>
 
+
+
 <template>
-    <div class="about">
-        <NavBar/>
-      <Message />
-      <PrivateMessage />
-    </div>
+  <div class="about">
+    <NavBar/>
+    <button @click="toggleChat">{{ showPrivateMessage ? 'Switch to Public Chat' : 'Switch to Private Chat' }}</button>
+    <Message v-if="!showPrivateMessage" />
+    <PrivateMessage v-if="showPrivateMessage" />
+  </div>
 </template>
