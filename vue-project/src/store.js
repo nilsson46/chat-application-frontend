a2fad95c77364ef7e92f1ea860cd147cdb5128d1
nilsson46 <nilssonsimon46@gmail.com>
2024-05-31 14:43:41 +0200

    Format after the changes

--- vue-project/src/store.js
+++ vue-project/src/store.js
@@ -1,13 +1,13 @@
 // store.js
-import Vuex from 'vuex';
-import { Client } from '@stomp/stompjs';
-import SockJS from 'sockjs-client';
-import axios from 'axios';
-import createPersistedState from 'vuex-persistedstate';
+import Vuex from "vuex";
+import { Client } from "@stomp/stompjs";
+import SockJS from "sockjs-client";
+import axios from "axios";
+import createPersistedState from "vuex-persistedstate";
 
 export default new Vuex.Store({
   state: {
-    socketUrl: 'http://localhost:9090',
+    socketUrl: "http://localhost:9090",
     socket: null,
     client: null,
     connected: false,
@@ -19,11 +19,18 @@ export default new Vuex.Store({
     publicSubscription: null,
     privateSubscription: null,
   },
-  
+
   plugins: [
     createPersistedState({
-      paths: ['username', 'publicMessages', 'privateMessages', 'token', 'isLoggedIn'],
-    })],
+      paths: [
+        "username",
+        "publicMessages",
+        "privateMessages",
+        "token",
+        "isLoggedIn",
+      ],
+    }),
+  ],
   mutations: {
     SET_SOCKET(state, socket) {
       state.socket = socket;
@@ -42,15 +49,18 @@ export default new Vuex.Store({
     },
     ADD_PRIVATE_MESSAGE(state, message) {
       state.privateMessages.push(message);
-      localStorage.setItem('privateMessages', JSON.stringify(state.privateMessages));
+      localStorage.setItem(
+        "privateMessages",
+        JSON.stringify(state.privateMessages)
+      );
     },
     SET_TOKEN(state, token) {
       state.token = token;
       if (token) {
-        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
         state.isLoggedIn = true;
       } else {
-        delete axios.defaults.headers.common['Authorization'];
+        delete axios.defaults.headers.common["Authorization"];
         state.isLoggedIn = false;
       }
     },
@@ -66,7 +76,7 @@ export default new Vuex.Store({
     },
   },
   getters: {
-    privateMessages: state => state.privateMessages,
+    privateMessages: (state) => state.privateMessages,
   },
   computed: {
     privateMessages() {
@@ -75,25 +85,25 @@ export default new Vuex.Store({
   },
   actions: {
     initializeStore({ commit }) {
-      const token = localStorage.getItem('token');
-      if (token===null) {
-        commit('SET_TOKEN', token);
+      const token = localStorage.getItem("token");
+      if (token === null) {
+        commit("SET_TOKEN", token);
       }
     },
     login({ commit, dispatch }, { username, token }) {
-      commit('SET_USERNAME', username);
-      commit('SET_TOKEN', token);
-      dispatch('connectWebSocket');
+      commit("SET_USERNAME", username);
+      commit("SET_TOKEN", token);
+      dispatch("connectWebSocket");
     },
     logout({ commit }) {
-      commit('RESET_STATE');
+      commit("RESET_STATE");
     },
     showErrorMessage({ commit }, errorMessage) {
-      commit('SET_ERROR_MESSAGE', errorMessage);
+      commit("SET_ERROR_MESSAGE", errorMessage);
     },
     connectWebSocket({ commit, state }) {
       const socket = new SockJS(`${state.socketUrl}/connect`);
-      commit('SET_SOCKET', socket);
+      commit("SET_SOCKET", socket);
 
       const client = new Client({
         webSocketFactory: () => state.socket,
@@ -101,21 +111,19 @@ export default new Vuex.Store({
           Authorization: `Bearer ${state.token}`,
         },
         onStompError: (frame) => {
-          // Handle the error
-          console.error('Received error from server:', frame.body);
-          if (frame.body.includes('Receiver not found')) {
-            console.error('The recipient username does not exist');
+          console.error("Received error from server:", frame.body);
+          if (frame.body.includes("Receiver not found")) {
+            console.error("The recipient username does not exist");
           }
         },
       });
-      
-      commit('SET_CLIENT', client);
+
+      commit("SET_CLIENT", client);
 
       client.onConnect = () => {
-        console.log('Connected to WebSocket server');
-        commit('SET_CONNECTED', true);
+        console.log("Connected to WebSocket server");
+        commit("SET_CONNECTED", true);
 
-        // Unsubscribe from the topics before subscribing again
         if (state.publicSubscription) {
           state.publicSubscription.unsubscribe();
         }
@@ -123,65 +131,78 @@ export default new Vuex.Store({
           state.privateSubscription.unsubscribe();
         }
 
-        state.publicSubscription = client.subscribe('/topic/public', (message) => {
-          if (message.body) {
-            let receivedPublicMessage = JSON.parse(message.body);
-            commit('ADD_PUBLIC_MESSAGE', receivedPublicMessage);
-            console.log('Received public message:', receivedPublicMessage);
+        state.publicSubscription = client.subscribe(
+          "/topic/public",
+          (message) => {
+            if (message.body) {
+              let receivedPublicMessage = JSON.parse(message.body);
+              commit("ADD_PUBLIC_MESSAGE", receivedPublicMessage);
+              console.log("Received public message:", receivedPublicMessage);
+            }
           }
-        });
-        
-        state.privateSubscription = client.subscribe(`/topic/private/${state.username}`, (message) => {
-          if (message.body) {
-            const receivedPrivateMessage = JSON.parse(message.body);
-            commit('ADD_PRIVATE_MESSAGE', receivedPrivateMessage);
-            console.log('Received private message:', receivedPrivateMessage);
+        );
+
+        state.privateSubscription = client.subscribe(
+          `/topic/private/${state.username}`,
+          (message) => {
+            if (message.body) {
+              const receivedPrivateMessage = JSON.parse(message.body);
+              commit("ADD_PRIVATE_MESSAGE", receivedPrivateMessage);
+              console.log("Received private message:", receivedPrivateMessage);
+            }
           }
-        });
+        );
       };
 
       client.activate();
     },
     sendPublicMessage({ state }, messageContent) {
       if (!state.connected) {
-        console.error('You are not connected to the WebSocket server');
+        console.error("You are not connected to the WebSocket server");
         return;
       }
 
       const message = {
         content: messageContent,
         sender: state.username,
-        receiver: 'PUBLIC',
-        type: 'PUBLIC'
+        receiver: "PUBLIC",
+        type: "PUBLIC",
       };
 
-      state.client.publish({ destination: '/ws/chat/sendMessage', body: JSON.stringify(message) });
-      console.log('Sent public message:', message);
+      state.client.publish({
+        destination: "/ws/chat/sendMessage",
+        body: JSON.stringify(message),
+      });
+      console.log("Sent public message:", message);
     },
-    sendPrivateMessage({ commit, state, dispatch }, { recipientUsername, privateMessageContent }) {
+    sendPrivateMessage(
+      { commit, state, dispatch },
+      { recipientUsername, privateMessageContent }
+    ) {
       if (!state.connected) {
-        console.error('You are not connected to the WebSocket server');
+        console.error("You are not connected to the WebSocket server");
         return;
       }
-      if(!state.token) {
-        console.error('You are not logged in');
-        dispatch('showErrorMessage', 'You are not logged in')
+      if (!state.token) {
+        console.error("You are not logged in");
+        dispatch("showErrorMessage", "You are not logged in");
         return;
       }
-    
+
       const message = {
         content: privateMessageContent,
         sender: state.username,
         receiver: recipientUsername,
-        type: 'PRIVATE'
+        type: "PRIVATE",
       };
-       
-      // Send the message to the private topic
-      state.client.publish({ destination: `/ws/chat/sendMessage/${recipientUsername}`, body: JSON.stringify(message) });
-      console.log('Sent private message:', message, recipientUsername);
-        
-      // Add the sent message to the privateMessages array
-      commit('ADD_PRIVATE_MESSAGE', message);
+
+      state.client.publish({
+        destination: `/ws/chat/sendMessage/${recipientUsername}`,
+        body: JSON.stringify(message),
+      });
+      console.log("Sent private message:", message, recipientUsername);
+
+      commit("ADD_PRIVATE_MESSAGE", message);
     },
   },
 });
